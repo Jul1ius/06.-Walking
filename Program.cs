2773ede9d1afa5c198f8752417c38d60ebab97e5
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-20 16:46:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stepsGoal = 10000;
+
+            int stepsToday = 0;
+            String input;
+
+            while (stepsToday < stepsGoal)
+            {
+                input = Console.ReadLine(); 
+
+                if (input == "Going home") 
+                {
+                    stepsToday += int.Parse(Console.ReadLine());
+                    break;
+                }
+
+                stepsToday += int.Parse(input);
+            }
+
+            if (stepsToday < stepsGoal)
+                Console.WriteLine($"{stepsGoal - stepsToday} more steps to reach goal.");
+            else
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{stepsToday - stepsGoal} steps over the goal!");
+            }
+        }
+    }
+}
